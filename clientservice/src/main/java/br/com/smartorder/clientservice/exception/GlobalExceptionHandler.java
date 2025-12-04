f3f86a13b564a0547f2f105a435284747e9264ca
GustavoQueiros <gustavo.dev47@gmail.com>
2025-12-04 20:05:13 -0300

    Criando exexception handle e eção generica

---
+++ clientservice/src/main/java/br/com/smartorder/clientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.smartorder.clientservice.exception;
+
+import br.com.smartorder.clientservice.dto.ErrorDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyUsedException.class)
+    public ResponseEntity<ErrorDTO> handleEmailAlreadyUsedException(EmailAlreadyUsedException exception) {
+        return ResponseEntity.badRequest().body(new ErrorDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleGenericException(Exception exception) {
+        //Todo: Implementar logs
+        return ResponseEntity.internalServerError().body(new ErrorDTO("An unexpected error ocurred"));
+    }
+
+}
