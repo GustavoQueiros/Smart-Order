f2af7bb27588ffc04402d692eb3078c1580fba43
GustavoQueiros <gustavo.dev47@gmail.com>
2025-12-05 20:39:04 -0300

    Feate:Criando metodo para deletar usuario

--- clientservice/src/main/java/br/com/smartorder/clientservice/controller/UserController.java
+++ clientservice/src/main/java/br/com/smartorder/clientservice/controller/UserController.java
@@ -4,14 +4,13 @@ package br.com.smartorder.clientservice.controller;
 import br.com.smartorder.clientservice.dto.UserRequestDto;
 import br.com.smartorder.clientservice.dto.UserResponseDto;
 import br.com.smartorder.clientservice.exception.EmailAlreadyUsedException;
+import br.com.smartorder.clientservice.exception.UserNotFoundException;
 import br.com.smartorder.clientservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +22,14 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponseDto> create(@RequestBody @Valid UserRequestDto request) throws EmailAlreadyUsedException {
         return ResponseEntity.ok(userService.create(request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws UserNotFoundException {
+
+        userService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
--- clientservice/src/main/java/br/com/smartorder/clientservice/service/UserService.java
+++ clientservice/src/main/java/br/com/smartorder/clientservice/service/UserService.java
@@ -4,6 +4,7 @@ import br.com.smartorder.clientservice.dto.UserRequestDto;
 import br.com.smartorder.clientservice.dto.UserResponseDto;
 import br.com.smartorder.clientservice.entity.UserEntity;
 import br.com.smartorder.clientservice.exception.EmailAlreadyUsedException;
+import br.com.smartorder.clientservice.exception.UserNotFoundException;
 import br.com.smartorder.clientservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,14 @@ public class UserService {
             return new UserResponseDto(userRepository.save(request.toEntity()));
         }
         throw new EmailAlreadyUsedException();
+
+    }
+
+    public void delete(Long id) throws UserNotFoundException {
+
+        UserEntity user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+
+        userRepository.delete(user);
 
     }
 
